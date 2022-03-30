bc80aeede51591a24e6d1eb8fc8620cc909bcd03
Hiyokokeko <sukepon0914@gmail.com>
2022-03-30 12:15:53 +0900

    fix:DNS関連、RDSの修正及びタスク定義のAPI_DOMAIN修正

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -35,12 +35,12 @@ resource "aws_db_parameter_group" "cs-db-parameter" {
 /* Database Instance */
 resource "aws_db_instance" "cs-db" {
   # identifier              = Endpoint of Database"
-  name                    = "production"
+  allocated_storage       = 20
+  instance_class          = "db.t2.micro"
   engine                  = "MySQL"
   engine_version          = "5.7.30"
-  instance_class          = "db.t2.micro"
-  allocated_storage       = 20
   storage_type            = "gp2"
+  name                    = "production"
   username                = var.aws_db_user
   password                = var.aws_db_password
   backup_retention_period = 7
@@ -61,7 +61,9 @@ resource "aws_db_instance" "cs-db" {
   lifecycle {
     prevent_destroy = false
   }
+
 }
+
 
 /* Subnet */
 resource "aws_db_subnet_group" "cs-rds-subnet-group" {
