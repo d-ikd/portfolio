7c852239f97b4f65bf4369053b3516f5a9d7b416
Hiyokokeko <sukepon0914@gmail.com>
2022-04-04 13:06:56 +0900

    test
    フロント:nuxt-webfontloader追加、store追加、不要ファイル削除

--- frontend/__tests__/index.spec.js
+++
@@ -1,9 +0,0 @@
-import Index from '../pages/index.vue'
-
-describe('pages/index.vue', () => {
-  it('has a default value', () => {
-    const defaultData = Index.data()
-    // subTitle の初期値をテストする
-    expect(defaultData.subTitle).toBe('Shinki-app is good service!!')
-  })
-})
