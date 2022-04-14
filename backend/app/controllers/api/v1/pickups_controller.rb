09e9ab3b2ab903209db1e11fdbabd130cb2d67f0
Hiyokokeko <sukepon0914@gmail.com>
2022-04-14 22:56:26 +0900

    add:models&controllers practices

---
+++ backend/app/controllers/api/v1/pickups_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class PickupsController < ApplicationController
+      def index
+        post = Pickup.all.includes(:post)
+        render json: post.as_json(include: :post)
+      end
+
+      private
+
+      def post_params
+        params.permit(:post_id)
+      end
+  end
+end
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,16 @@ module Api
       end
 
       def show
-
+        @user = User.includes({ postlike: :like_users },
+                              { reviews: [:post, :user, { review_likes: :user },] },
+                              { like_reviews: [:post, :user, { review_likes: :user },] },
+                              :followings, :followers).find(params[:id])
+        render json: @user.as_json(include: [{ postlike: { include: { like_users: { only: [:id] } } } },
+                                              { reviews: { include: [{ review_likes: { include: [{ user: { only: %w[id image name] }},] } }, { post: { only: %i[id name image] } },
+                                              { user: { only: %i[id name image] } }] } },
+                                              { like_reviews: { include: [{ review_likes: { include: [{ user: { only: %w[id image name] }},] } }, { post: { only: %i[id name image] } },
+                                              { user: { only: %i[id name image] } }] } },
+                                              :followings, :followers])
       end
 
       def destroy
@@ -16,6 +25,13 @@ module Api
           render json: user
         else
           render json: { status: 400 }
+        end
+      end
+
+      def search
+        if params[:search]
+          @user = User.search(params[:search]).includes(:followings, :followers)
+          render json: @user.as_json(include: %i[followings followers])
         end
       end
     end
