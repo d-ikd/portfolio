96379e9ee7b951101a54099440976853abf76667
Hiyokokeko <sukepon0914@gmail.com>
2022-04-21 10:59:36 +0900

    create:編集画面作成

--- frontend/pages/users/edit.vue
+++ frontend/pages/users/edit.vue
@@ -1,66 +1,69 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col xl="5" lg="7" sm="9" cols="12">
-        <v-card class="mx-auto">
-          <v-tabs
-            v-model="tab"
-            fixed-tabs
-            centered
-            background-color="grey lighten-2"
-            slider-color="info"
-            icons-and-text
-          >
-            <v-tab class="ma-0 pa-0">
-              プロフィール
-              <v-icon>mdi-account</v-icon>
-            </v-tab>
-            <v-tab class="ma-0 pa-0">
-              登録情報
-              <v-icon>mdi-account-cog</v-icon>
-            </v-tab>
-          </v-tabs>
-          <v-tabs-items v-model="tab" touchless>
-            <v-container>
-              <v-tab-item class="px-2">
-                <!-- <edit-avatar /> -->
-                <v-divider />
-                <!-- <edit-profile /> -->
-              </v-tab-item>
-              <v-tab-item class="px-2">
-                <!-- <edit-email /> -->
-                <v-divider />
-                <!-- <edit-password /> -->
-                <v-divider />
-                <!-- <delete-user /> -->
-              </v-tab-item>
-            </v-container>
-          </v-tabs-items>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-container>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">
+            メールアドレス変更
+          </h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" lazy-validation>
+            <v-text-field
+              v-model="user.email"
+              prepend-icon="mdi-email"
+              label="新しいメールアドレス"
+            />
+            <v-text-field
+              v-model="user.password"
+              prepend-icon="mdi-lock"
+              append-icon="mdi-eye-off"
+              label="パスワード"
+            />
+            <v-card-actions>
+              <v-btn
+                color="light-green darken-1"
+                class="white--text"
+                @click="editEmail"
+              >
+                保存する
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// import editAvatar from '~/components/editUser/EditAvatar'
-// import editEmail from "~/components/editUser/EditEmail"
-// import editPassword from "~/components/editUser/EditPassword"
-// import editProfile from "~/components/editUser/EditProfile"
-// import deleteUser from "~/components/editUser/DeleteUser"
-
 export default {
-  components: {
-    // editAvatar,
-    // editEmail,
-    // editPassword,
-    // editProfile,
-    // deleteUser,
-  },
   data() {
     return {
-      tab: null,
+      user: {
+        password: '',
+        email: '',
+      },
     }
+  },
+  methods: {
+    editEmail() {
+      this.$axios
+        .put('api/v1/auth', this.user, {
+          headers: {
+            'access-token': localStorage.getItem('access-token'),
+            uid: localStorage.getItem('uid'),
+            client: localStorage.getItem('client'),
+          },
+        })
+        .then((response) => {
+          localStorage.setItem('access-token', response.headers['access-token'])
+          localStorage.setItem('client', response.headers.client)
+          localStorage.setItem('uid', response.headers.uid)
+          localStorage.setItem('token-type', response.headers['token-type'])
+          window.location.href = '/'
+        })
+    },
   },
 }
 </script>
