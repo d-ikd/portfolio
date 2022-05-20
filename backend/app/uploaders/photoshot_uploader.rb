b599f9aff0213f7cc6a5bd92edc62c115ac18abf
Hiyokokeko <sukepon0914@gmail.com>
2022-05-20 14:15:34 +0900

    fix:Postsモデル、コントローラ編集
    uploaders追記

--- backend/app/uploaders/photoshot_uploader.rb
+++ backend/app/uploaders/photoshot_uploader.rb
@@ -1,9 +1,7 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class PhotoshotUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  # include CarrierWave::MiniMagick
-  # process resize_to_fill: [100, 100, 'Center']
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
@@ -50,6 +48,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
     original_filename
-    #   "something.jpg" if original_filename
+    # "something.jpg" if original_filename
   end
 end
